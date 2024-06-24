e88ae756110e7eb6a9ab0183e3b7686bafafefa0
lwr <944604889@qq.com>
2024-06-24 09:40:30 +0800

    第一个主页 defer加载js，js内部变量匿名，css移到外部，调整导航样式

---
+++ js/nav-demo.js
@@ -0,0 +1,28 @@
+(() => {
+
+    let fid = document.getElementById("fid");
+
+    let map = new Map();
+    map.set("wyy", "https://music.163.com/#/song?id=2130796760");
+    map.set("ryf", "https://www.ruanyifeng.com/blog/");
+    map.set("git", "https://learngitbranching.js.org/?locale=zh_CN");
+    map.set("dy", "https://www.douyin.com/");
+
+    //节流
+    let timer
+    function flowD(action) {
+        if (!timer) {
+            fid.src = map.get(action);
+            timer = setTimeout(() => {
+                timer = null;
+            }, 2000);
+        }
+    }
+
+    let arr = [document.querySelector(".nav .one"), document.querySelector(".header .one")];
+    arr.forEach(elem => elem.addEventListener("click", (e) => {
+        if (e.target.dataset.action) {
+            flowD(e.target.dataset.action);
+        }
+    }));
+})();
